59f82eb534cd1d9c4c39e4dddeadec5f69566983
Raúl Villares <raulvillares@gmail.com>
2024-02-25 22:28:33 +0100

    manual deploy

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,6 @@
 import { Application } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
-import TabsController from "/javascripts/tabs_controller.js"
-import DropdownController from "/javascripts/dropdown_controller.js"
+import TabsController from "./tabs_controller.js"
+import DropdownController from "./dropdown_controller.js"
 
 window.Stimulus = Application.start()
 Stimulus.register("dropdown", DropdownController)
